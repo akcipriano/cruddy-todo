6689bbb7e36f4d35436b0d25f98c7d0b56965e97
Alyssa Cipriano <alyssacipriano@gmail.com>
2019-02-01 15:50:12 -0800

    Complete deleting a todo

--- datastore/index.js
+++ datastore/index.js
@@ -3,25 +3,19 @@ const path = require('path');
 const _ = require('underscore');
 const counter = require('./counter');
 
-
-var items = {};
-
 // Public API - Fix these CRUD functions ///////////////////////////////////////
 
 exports.create = (text, callback) => {
   counter.getNextUniqueId((err, id) => {
-    // var path = `${exports.dataDir}/${id}.txt`;
     var filePath = path.join(exports.dataDir, `${id}.txt`);
     fs.writeFile(filePath, text, 'utf8', (err) => {
       if (err) {
         callback(err, null);
       } else {
-        console.log('----->', text);
         callback(null, { id, text});
       }
     });
   });
-
 };
 
 exports.readAll = (callback) => {
@@ -72,20 +66,29 @@ exports.update = (id, text, callback) => {
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+  fs.readdir(exports.dataDir, (err, files) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      if (!files.includes(id + '.txt')) {
+        callback(new Error(`No item with id: ${id}`));
+      } else {
+        var filePath = path.join(exports.dataDir, `${id}.txt`);
+        fs.unlink(filePath, (err) => {
+          if (err) {
+            callback(err, null);
+          } else {
+            callback();
+          }
+        });
+      }
+    }
+  });
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
 
 exports.dataDir = path.join(__dirname, 'data');
-console.log('--------->', exports.dataDir);
 
 exports.initialize = () => {
   if (!fs.existsSync(exports.dataDir)) {
